26f5546c6a5cb9e797d9fc1eeda2cd18ec82bbe7
nskarve <32215122+nskarve@users.noreply.github.com>
2024-10-29 23:20:40 +0200

    other

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World N!");
+﻿Console.WriteLine("Hello, World N!");
 Decimal num1 = 6;
 Decimal num2 = 4;
 Decimal num3 = num1 * num2;
 Console.WriteLine($" {num1} * {num2}  equals to {num3}!");
 Console.WriteLine($" {num1} + {num2}  equals to {num3}!");
 
-var names = new List <string> {"Ge", "ge"};
+var names = new List <string> {"Nikolaos", "Giorgos"};
 foreach (var name in names) {
-    Console.WriteLine($"The namaew are {name.ToLower()}");
+    Console.WriteLine($"The names are {name.ToUpper()} \"--\" {name.ToLower()}");
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97da2d1dd06ed453080fbee179ec1c761f083bb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cecd0cf3902f4b717c07dce749feab29b96a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
