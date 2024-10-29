97da2d1dd06ed453080fbee179ec1c761f083bb0
nskarve <32215122+nskarve@users.noreply.github.com>
2024-10-29 20:07:47 +0200

    other

--- Program.cs
+++ Program.cs
@@ -4,4 +4,4 @@ Decimal num1 = 6;
 Decimal num2 = 4;
 Decimal num3 = num1 * num2;
 Console.WriteLine($" {num1} * {num2}  equals to {num3}!");
-
+Console.WriteLine($" {num1} + {num2}  equals to {num3}!");
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3246cb4ee9c730ce3cb8ff0ce4795e9dfdb0c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6734521aeb09fa9f5b56eafb7925a96c1a1dbd3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
