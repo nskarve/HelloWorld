6734521aeb09fa9f5b56eafb7925a96c1a1dbd3b
Nikolaos M. Skarvelis <nikos.m.skarvelis@gmail.com>
2024-10-29 03:03:24 +0200

    Another example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World N!");
+Decimal num1 = 6;
+Decimal num2 = 4;
+Decimal num3 = num1 * num2;
+Console.WriteLine($" {num1} * {num2}  equals to {num3}!");
+
