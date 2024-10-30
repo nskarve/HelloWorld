c4588edf0229dfead5f5a6a58b6cd9ff95105608
nskarve <32215122+nskarve@users.noreply.github.com>
2024-10-31 00:56:03 +0200

    20241031

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cecd0cf3902f4b717c07dce749feab29b96a6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f5546c6a5cb9e797d9fc1eeda2cd18ec82bbe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
